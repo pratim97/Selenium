c72f6b4d45798f7d2cc3992a829689944279f6e4
pratim97 <raypratim209@gmail.com>
2022-08-20 13:45:51 +0530

    Changes made to file

---
+++ selenium_1.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.service import Service
+
+se = Service(executable='D:\selenium\chromedriver')
+driver = webdriver.Chrome(service=se)
+
+url='http://demostore.supersqa.com'
+driver.get(url)
+
